5bd064062b084998f37cf88f6db8f130326f21de
RoshanSutihar <43160931+RoshanSutihar@users.noreply.github.com>
2022-10-17 21:03:47 -0500

    Update Patternpractice.java

--- Patternpractice.java
+++ Patternpractice.java
@@ -3,49 +3,8 @@ package patternpractice;
 import java.util.Scanner;
 public class Patternpractice {
 
-    public static void main(String[] args) {
-       
-        
-        for(int i =1; i<=5; i++){
-            for (int col = 1; col<=i; col++){
-            
-            System.out.print("x ");
-            }
-            
-            System.out.println();
-        }
-        
-         for ( int row = 1; row <=5; row++){
-    
-            for (int col = row; col<=5; col++){
-            System.out.print("x ");
-            }
-            System.out.println();
-            } 
-        System.out.println();
-         
-//         ==============================================================
-
-         for (int row1 = 1; row1 <=5; row1++){
-         
-         for (int rowb = row1; rowb<=5; rowb++){
-         
-             System.out.print("  ");
-             
-         }
-         for (int col1 = 1; col1<=row1; col1++){
-         
-         System.out.print(" x");
-         
-         }
-         
-        
-         
-         System.out.println();
-         }
-        System.out.print("");
-         //    =================================================================
-    
+    public static void main(String[] args) 
+   
     
          System.out.println("0        1         2");
          System.out.println("12345678901234567890");
@@ -76,10 +35,7 @@ public class Patternpractice {
       Scanner scan = new Scanner(System.in);
       
       
-      System.out.print("Enter amount: ");
-      double inpAmnt = scan.nextDouble();
-      
-      double realAmnt = inpAmnt*100;
+     
       
       
       
